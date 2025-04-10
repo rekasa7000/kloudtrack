d3e396328eec9c94de86863f963ab346811dec22
Regee <regeecasana57@gmail.com>
2025-04-10 20:45:38 +0800

    adding jsonwebtoken

--- server/src/config/config.ts
+++ server/src/config/config.ts
@@ -3,13 +3,15 @@ import dotenv from "dotenv";
 dotenv.config();
 
 interface Config {
-  port: number;
-  nodeEnv: string;
+  PORT: number;
+  NODE_ENV: string;
+  JWT_SECRET: string;
 }
 
 const config: Config = {
-  port: Number(process.env.PORT) || 3000,
-  nodeEnv: process.env.NODE_ENV || "development",
+  PORT: Number(process.env.PORT) || 3000,
+  NODE_ENV: process.env.NODE_ENV || "development",
+  JWT_SECRET: process.env.JWT_SECRET || "",
 };
 
 export default config;
